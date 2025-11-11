4e3ed7f9e6a97f9a1c9efee1c720c28f4ccfa0ed
yashdive <yashdive190@gmail.com>
2025-11-11 18:45:51 -0500

    env changes

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: '/api', // Relative URL for production
+  apiUrl: 'https://snapclass-rubrics-api.onrender.com/api',
   apiTimeout: 60000, // 60 seconds
 };
